f34e36901fa801210cb98fd63e35a67b76731f79
lavanya <bheemunilavanya@gmail.com>
2021-06-20 19:08:11 +0530

    first commit

---
+++ for/src/forpackage/fr.java
@@ -0,0 +1,15 @@
+package forpackage;
+
+public class fr {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=5;i++)
+		{
+			System.out.println("hi");
+			System.out.println("hello");
+			}
+		
+	}
+
+}
