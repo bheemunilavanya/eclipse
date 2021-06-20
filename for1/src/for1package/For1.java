37db91552c5986485a4ffb1d036ba7dbc30c9212
lavanya <bheemunilavanya@gmail.com>
2021-06-20 19:15:06 +0530

    fs commit

---
+++ for1/src/for1package/For1.java
@@ -0,0 +1,15 @@
+package for1package;
+
+public class For1 {
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=5;i++)
+		{
+			System.out.println("hi");
+			System.out.println("hello");
+			}
+		
+	}
+
+
+}
